aefc259b2b07b6122430df7742410057a3e7958f
Evan Sosenko <razorx@evansosenko.com>
2015-12-13 12:21:27 -0800

    Align test code for days 1 and 2

--- test/adventofcode/day_01_test.clj
+++ test/adventofcode/day_01_test.clj
@@ -3,23 +3,23 @@
             [adventofcode.day-01 :refer :all]))
 
 (deftest final-floor-test
-  (is (= 0 (final-floor "(())")))
-  (is (= 0 (final-floor "()()")))
-  (is (= 3 (final-floor "(((")))
-  (is (= 3 (final-floor "(()(()(")))
-  (is (= 3 (final-floor "))(((((")))
+  (is (=  0 (final-floor "(())")))
+  (is (=  0 (final-floor "()()")))
+  (is (=  3 (final-floor "(((")))
+  (is (=  3 (final-floor "(()(()(")))
+  (is (=  3 (final-floor "))(((((")))
   (is (= -1 (final-floor "())")))
   (is (= -1 (final-floor "))(")))
   (is (= -3 (final-floor ")))")))
   (is (= -3 (final-floor ")())())"))))
 
 (deftest paren-to-int-test
-  (is (= 1 (paren-to-int "(")))
+  (is (=  1 (paren-to-int "(")))
   (is (= -1 (paren-to-int ")")))
-  (is (= 0 (paren-to-int "a"))))
+  (is (=  0 (paren-to-int "a"))))
 
 (deftest steps-to-basement-test
-  (is (= 1 (steps-to-basement ")")))
-  (is (= 1 (steps-to-basement "))")))
-  (is (= 5 (steps-to-basement "()())")))
+  (is (=  1 (steps-to-basement ")")))
+  (is (=  1 (steps-to-basement "))")))
+  (is (=  5 (steps-to-basement "()())")))
   (is (= 11 (steps-to-basement "(()()()()))))))(("))))
--- test/adventofcode/day_02_test.clj
+++ test/adventofcode/day_02_test.clj
@@ -8,11 +8,11 @@
 (deftest paper-test
   (is (= 58 (paper [2 3 4])))
   (is (= 43 (paper [1 1 10])))
-  (is (= 7 (paper [1 1 1])))
+  (is (= 7  (paper [1 1 1])))
   (is (= 58 (paper [4 2 3]))))
 
 (deftest ribbon-test
   (is (= 34 (ribbon [2 3 4])))
   (is (= 14 (ribbon [1 1 10])))
-  (is (= 5 (ribbon [1 1 1])))
+  (is (= 5  (ribbon [1 1 1])))
   (is (= 34 (ribbon [4 2 3]))))
