dbf5ca428dc96857f9311210ea6b0dacd65c6ae6
Evan Sosenko <razorx@evansosenko.com>
2015-12-13 15:59:50 -0800

    Solve day 5 part 1

--- src/adventofcode/day_05.clj
+++ src/adventofcode/day_05.clj
@@ -1,6 +1,16 @@
 (ns adventofcode.day-05)
 
+(defn nice?
+  "Checks if a string is nice."
+  [string]
+  (if (and
+       (re-find #"(\w)\1" string)
+       (< 2 (count (re-seq #"[aeiou]" string)))
+       (nil? (re-find #"(ab|cd|pq|xy)" string)))
+    true false))
+
 (defn solve
   "Given the input for the day, returns the solution."
   [input]
-  "")
+  (let [strings (clojure.string/split-lines input)]
+    [(get (frequencies (map nice? strings)) true)]))
--- test/adventofcode/day_05_test.clj
+++ test/adventofcode/day_05_test.clj
@@ -1,3 +1,10 @@
 (ns adventofcode.day-05-test
   (:require [clojure.test :refer :all]
             [adventofcode.day-05 :refer :all]))
+
+(deftest nice?-test
+  (is (true?  (nice? "ugknbfddgicrmopn")))
+  (is (true?  (nice? "aaa")))
+  (is (false? (nice? "jchzalrnumimnmhp")))
+  (is (false? (nice? "haegwjzuvuyypxyu")))
+  (is (false? (nice? "dvszwmarrgswjxmb"))))
