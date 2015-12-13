05f0e82f2b72011c919744c1ae6b232dc067d3d5
Evan Sosenko <razorx@evansosenko.com>
2015-12-13 13:13:23 -0800

    Solve day 3 part 1

--- src/adventofcode/day_03.clj
+++ src/adventofcode/day_03.clj
@@ -1,6 +1,30 @@
 (ns adventofcode.day-03)
 
+(defn move-to-vector
+  "Converts a move (><^v) to a vector that represents the move,
+  e.g., > becomes [1 0] and v becomes [0 -1].
+  Anything else is converted to [0 0]."
+  [move]
+  (case move
+    ">" [1 0]
+    "<" [-1 0]
+    "^" [0 1]
+    "v" [0 -1]
+    [0 0]))
+
+(defn moves-to-seq
+  "Converts a string of moves to a seq of vectors using move-to-vector."
+  [moves]
+  (map (comp move-to-vector str) (seq moves)))
+
+(defn track-houses
+  "Given a seq of moves as vectors, returns a set of all visited houses."
+  [moves]
+  (last (reduce #(let [next-house (map + (first %1) %2)]
+                   [next-house (conj (last %1) next-house)])
+                [[0 0] #{[0 0]}] moves)))
+
 (defn solve
   "Given the input for the day, returns the solution."
   [input]
-  "")
+  [((comp count track-houses moves-to-seq) input)])
--- test/adventofcode/day_03_test.clj
+++ test/adventofcode/day_03_test.clj
@@ -1,3 +1,18 @@
 (ns adventofcode.day-03-test
   (:require [clojure.test :refer :all]
             [adventofcode.day-03 :refer :all]))
+
+(deftest move-to-vector-test
+  (is (= [1 0] (move-to-vector ">")))
+  (is (= [-1 0] (move-to-vector "<")))
+  (is (= [0 1] (move-to-vector "^")))
+  (is (= [0 -1] (move-to-vector "v")))
+  (is (= [0 0] (move-to-vector "a"))))
+
+(deftest moves-to-seq-test
+  (is (= '([1 0] [-1 0] [0 1] [0 -1])
+         (moves-to-seq "><^v"))))
+
+(deftest track-houses-test
+  (is (= #{[0 0] [1 0] [2 0] [1 -1]}
+         (track-houses [[1 0] [1 0] [-1 0] [0 -1]]))))
