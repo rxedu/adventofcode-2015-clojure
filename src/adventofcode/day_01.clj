9d3cec3aeb994dc4f836b391a3bffe2cf00ee7df
Evan Sosenko <razorx@evansosenko.com>
2015-12-12 21:56:19 -0800

    Solve day 1 part 1

--- src/adventofcode/day_01.clj
+++ src/adventofcode/day_01.clj
@@ -1,6 +1,14 @@
 (ns adventofcode.day-01)
 
+(defn final-floor
+  "Finds the floor Santa ends on."
+  [directions]
+  (let [[ups downs]
+        (split-with (partial = "(") (map str (seq (sort directions))))]
+    (- (count ups) (count downs))))
+
 (defn solve
   "Given the input for the day, returns the solution."
   [input]
-  "")
+  (let [directions (clojure.string/trim-newline input)]
+    (str [(final-floor directions)])))
--- test/adventofcode/day_01_test.clj
+++ test/adventofcode/day_01_test.clj
@@ -1,3 +1,14 @@
 (ns adventofcode.day-01-test
   (:require [clojure.test :refer :all]
             [adventofcode.day-01 :refer :all]))
+
+(deftest final-floor-test
+  (is (= 0 (final-floor "(())")))
+  (is (= 0 (final-floor "()()")))
+  (is (= 3 (final-floor "(((")))
+  (is (= 3 (final-floor "(()(()(")))
+  (is (= 3 (final-floor "))(((((")))
+  (is (= -1 (final-floor "())")))
+  (is (= -1 (final-floor "))(")))
+  (is (= -3 (final-floor ")))")))
+  (is (= -3 (final-floor ")())())"))))
