8a459b1a29cfdd67cc2a1e779fedf5522d1cdf1c
Evan Sosenko <razorx@evansosenko.com>
2015-12-13 01:55:25 -0800

    Solve day 2 part 1

--- src/adventofcode/day_02.clj
+++ src/adventofcode/day_02.clj
@@ -1,6 +1,20 @@
 (ns adventofcode.day-02)
 
+(defn input-to-dimensions
+  "Converts a input string of the form 4x2x6
+  to a vector [4 2 6]"
+  [input-string]
+  (map #(Integer. %) (clojure.string/split input-string #"x")))
+
+(defn paper
+  "Computes the total paper needed to wrap a present."
+  [dimensions]
+  (let [[x y z] (sort dimensions)]
+    (+ (* 3 x y) (* 2 x z) (* 2 y z))))
+
 (defn solve
   "Given the input for the day, returns the solution."
   [input]
-  "")
+  (let [all-dimensions
+        (map input-to-dimensions (clojure.string/split-lines input))]
+    [(reduce + (map paper all-dimensions))]))
--- test/adventofcode/day_02_test.clj
+++ test/adventofcode/day_02_test.clj
@@ -1,3 +1,12 @@
 (ns adventofcode.day-02-test
   (:require [clojure.test :refer :all]
             [adventofcode.day-02 :refer :all]))
+
+(deftest input-to-dimensions-test
+  (is (= [4 2 6] (input-to-dimensions "4x2x6"))))
+
+(deftest paper-test
+  (is (= 58 (paper [2 3 4])))
+  (is (= 43 (paper [1 1 10])))
+  (is (= 7 (paper [1 1 1])))
+  (is (= 58 (paper [4 2 3]))))
