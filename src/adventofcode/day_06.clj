4cb88ab9de6d369fb02218f28c08863fa4042be0
Evan Sosenko <razorx@evansosenko.com>
2015-12-13 20:03:11 -0800

    Solve day 6 part 1

--- src/adventofcode/day_06.clj
+++ src/adventofcode/day_06.clj
@@ -1,6 +1,54 @@
 (ns adventofcode.day-06)
 
+(def actions
+  {:on (constantly true) :off (constantly false) :toggle #(not %)})
+
+(defn string-to-instruction
+  "Converts an instruction from a string to a vector,
+  e.g., turn on 0,5 through 2,10 becomes [:on [0 2] [5 10]]."
+  [string]
+  (let [matches
+        (rest (re-matches
+               #"^(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)$"
+               string))
+        command (keyword (clojure.string/replace (first matches) "turn " ""))
+        lights (map #(Integer. %) (rest matches))]
+    [command (take-nth 2 lights) (take-nth 2 (rest lights))]))
+
+(defn init-grid
+  "Returns a lazy n-by-n grid where every element is a the given constant."
+  [n constant]
+  (repeat n (repeat n constant)))
+
+(defn form-grid
+  "Returns an n-by-n grid where the state of the light at position [x y]
+  is determined by the value of the state function on that location."
+  [n state-fn]
+  (let [grid-size (range n)]
+    (reduce
+     (fn [row x] (conj row (reduce
+                            (fn [column y] (conj column (state-fn [x y])))
+                            [] grid-size)))
+     [] grid-size)))
+
+(defn modify-grid
+  "Applies an instruction to a grid of lights and returns the modified grid."
+  [grid instruction actions]
+  (let [action ((first instruction) actions)
+        [xr yr] (rest instruction)]
+    (form-grid
+     (count grid)
+     (fn [[x y]]
+       (let [light (nth (nth grid x) y)]
+         (if (and (<= (first xr) x (last xr))
+                  (<= (first yr) y (last yr)))
+           (action light) light))))))
+
 (defn solve
   "Given the input for the day, returns the solution."
   [input]
-  "")
+  (let [instructions (map string-to-instruction
+                          (clojure.string/split-lines input))]
+    [((comp #(get % true) frequencies flatten)
+      (reduce #(modify-grid %1 %2 actions)
+              (init-grid 1000 false) instructions))]))
--- test/adventofcode/day_06_test.clj
+++ test/adventofcode/day_06_test.clj
@@ -1,3 +1,40 @@
 (ns adventofcode.day-06-test
   (:require [clojure.test :refer :all]
             [adventofcode.day-06 :refer :all]))
+
+(deftest string-to-instruction-test
+  (is (= [:on [0 999] [0 999]]
+         (string-to-instruction "turn on 0,0 through 999,999")))
+  (is (= [:toggle [0 999] [0 0]]
+         (string-to-instruction "toggle 0,0 through 999,0")))
+  (is (= [:off [499 500] [499 500]]
+         (string-to-instruction "turn off 499,499 through 500,500"))))
+
+(deftest init-grid-test
+  (is (= [[false false] [false false]]
+         (init-grid 2 false)))
+  (is (= [[0 0 0] [0 0 0] [0 0 0]]
+         (init-grid 3 0))))
+
+(deftest form-grid-test
+  (is (= [[true  true]
+          [false false]]
+         (form-grid 2 (fn [[x y]] (if (= x 0) true false)))))
+  (is (= [[false true]
+          [false true]]
+         (form-grid 2 (fn [[x y]] (if (= y 1) true false)))))
+  (is (= [[false false false]
+          [false false true]
+          [false false false]]
+         (form-grid 3 (fn [[x y]] (if (and (= x 1) (= y 2)) true false))))))
+
+(deftest modify-grid-test
+  (let [grid (init-grid 3 false)]
+    (is (= [[true  false false]
+            [true  false false]
+            [false false false]]
+           (modify-grid grid [:on [0 1] [0 0]] actions)))
+    (is (= [[true  true  true]
+            [false false false]
+            [false false false]]
+           (modify-grid grid [:toggle [0 0] [0 2]] actions)))))
