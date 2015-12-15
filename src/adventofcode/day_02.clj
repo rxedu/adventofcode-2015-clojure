9f26895480358035a5729d096ab71b248e3131fc
Evan Sosenko <razorx@evansosenko.com>
2015-12-15 11:42:21 -0800

    Edit some day 2 docstrings

--- src/adventofcode/day_02.clj
+++ src/adventofcode/day_02.clj
@@ -1,19 +1,18 @@
 (ns adventofcode.day-02)
 
 (defn parse-dimensions
-  "Converts a input string of the form 4x2x6
-  to a vector [4 2 6]."
+  "Converts a input string of the form 4x2x6 to a vector [4 2 6]."
   [input-string]
   (map #(Integer. %) (clojure.string/split input-string #"x")))
 
 (defn paper
-  "Computes the total paper needed to wrap a present."
+  "Computes the total paper needed to wrap a present of dimensions [x y z]."
   [dimensions]
   (let [[x y z] (sort dimensions)]
     (+ (* 3 x y) (* 2 x z) (* 2 y z))))
 
 (defn ribbon
-  "Computes the total ribbon needed to wrap a present."
+  "Computes the total ribbon needed to wrap a present of dimensions [x y z]."
   [dimensions]
   (let [[x y z] (sort dimensions)]
     (+ (* 2 x) (* 2 y) (* x y z))))
