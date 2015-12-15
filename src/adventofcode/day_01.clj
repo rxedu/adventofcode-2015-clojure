d186cee3e127600006cb70c9c3f7ce2f0c397835
Evan Sosenko <razorx@evansosenko.com>
2015-12-15 12:11:18 -0800

    Test solve directly for day 1

--- src/adventofcode/day_01.clj
+++ src/adventofcode/day_01.clj
@@ -10,22 +10,18 @@
   [directions]
   (replace direction-strings (map str (seq directions))))
 
-(defn final-floor
-  "Given a string of directions, finds the floor Santa ends on."
-  [directions]
-  (reduce + (parse-directions directions)))
-
 (defn steps-to-basement
   "Finds the first step that takes Santa to the basement."
   [directions]
   (count (take-while (complement nil?)
                      (reductions
                       #(if (= %1 -1) (reduced nil) (+ %1 %2))
-                      (parse-directions directions)))))
+                      directions))))
 
 (defn solve
   "Given the input for the day, returns the solution."
   [input]
-  (let [directions (clojure.string/trim-newline input)]
-    [(final-floor directions)
+  (let [directions
+        ((comp parse-directions clojure.string/trim-newline) input)]
+    [(reduce + directions)
      (steps-to-basement directions)]))
--- test/adventofcode/day_01_test.clj
+++ test/adventofcode/day_01_test.clj
@@ -5,19 +5,23 @@
 (deftest parse-directions-test
   (is (=  [1 -1 1 1 1 -1] (parse-directions "()((()"))))
 
-(deftest final-floor-test
-  (is (=  0 (final-floor "(())")))
-  (is (=  0 (final-floor "()()")))
-  (is (=  3 (final-floor "(((")))
-  (is (=  3 (final-floor "(()(()(")))
-  (is (=  3 (final-floor "))(((((")))
-  (is (= -1 (final-floor "())")))
-  (is (= -1 (final-floor "))(")))
-  (is (= -3 (final-floor ")))")))
-  (is (= -3 (final-floor ")())())"))))
-
 (deftest steps-to-basement-test
-  (is (=  1 (steps-to-basement ")")))
-  (is (=  1 (steps-to-basement "))")))
-  (is (=  5 (steps-to-basement "()())")))
-  (is (= 11 (steps-to-basement "(()()()()))))))(("))))
+  (is (= 1 (steps-to-basement [-1])))
+  (is (= 9 (steps-to-basement [1 1 1 -1 1 -1 -1 -1 -1 -1 -1 1]))))
+
+(deftest solve-test
+  (testing "part 1"
+    (is (=  0 (first (solve "(())"))))
+    (is (=  0 (first (solve "()()"))))
+    (is (=  3 (first (solve "((("))))
+    (is (=  3 (first (solve "(()(()("))))
+    (is (=  3 (first (solve "))((((("))))
+    (is (= -1 (first (solve "())"))))
+    (is (= -1 (first (solve "))("))))
+    (is (= -3 (first (solve ")))"))))
+    (is (= -3 (first (solve ")())())")))))
+  (testing "part 2"
+    (is (=  1 (second (solve ")"))))
+    (is (=  1 (second (solve "))"))))
+    (is (=  5 (second (solve "()())"))))
+    (is (= 11 (second (solve "(()()()()))))))(("))))))
