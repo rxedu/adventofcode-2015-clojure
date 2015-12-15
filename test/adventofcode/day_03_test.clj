8d3bc138ab1ab5f83e2253bdf2fb7cc437e11937
Evan Sosenko <razorx@evansosenko.com>
2015-12-15 12:34:35 -0800

    Add solve-test for day 3

--- test/adventofcode/day_03_test.clj
+++ test/adventofcode/day_03_test.clj
@@ -11,3 +11,13 @@
 (deftest track-houses-test
   (is (= #{[0 0] [1 0] [2 0] [1 -1]}
          (track-houses [[1 0] [1 0] [-1 0] [0 -1]]))))
+
+(deftest solve-test
+  (testing "part 1"
+    (is (= 2 (first (solve ">"))))
+    (is (= 4 (first (solve "^>v<"))))
+    (is (= 2 (first (solve "^v^v^v^v^v")))))
+  (testing "part 2"
+    (is (= 3  (second (solve "^v"))))
+    (is (= 3  (second (solve "^>v<"))))
+    (is (= 11 (second (solve "^v^v^v^v^v"))))))
