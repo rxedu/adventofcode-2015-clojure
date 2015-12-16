6a7be9dad8a1775437456daa241258a2dea84572
Evan Sosenko <razorx@evansosenko.com>
2015-12-16 00:57:06 -0800

    Solve day 9 part 1

--- src/adventofcode/day_09.clj
+++ src/adventofcode/day_09.clj
@@ -1,6 +1,40 @@
-(ns adventofcode.day-09)
+(ns adventofcode.day-09
+  (:require [clojure.math.combinatorics]))
+
+(defn parse-connection
+  "Converts a connection from a string to the form {#{:a :b} distance},
+  e.g., Alpha to Beta = 100 becomes {#{:alpha :beta} 100}."
+  [string]
+  (let [matches
+        ((comp reverse rest) (re-matches #"(\w+) to (\w+) = (\d+)" string))]
+    (assoc {}
+           (set (map (comp keyword clojure.string/lower-case) (rest matches)))
+           (Integer. (first matches)))))
+
+(defn locations
+  "Given a map of connections, returns a set of locations."
+  [connections]
+  (set (apply concat (keys connections))))
+
+(defn distance
+  "Given a map of connections, returns the total distance for a route."
+  [connections route]
+  (first (reduce (fn [[dist pre] nxt]
+                   [(+ dist (connections (set [pre nxt]))) nxt])
+                 [0 (first route)] (rest route))))
+
+(defn distances
+  "Given a seq of connections, returns the set of distances
+  for all routes that visit each location exactly once."
+  [connections]
+  (set (map (partial distance connections)
+            (clojure.math.combinatorics/permutations
+             (locations connections)))))
 
 (defn solve
   "Given the input for the day, returns the solution."
   [input]
-  "")
+  (let [connections
+        (into {} (map parse-connection
+                      (clojure.string/split-lines input)))]
+    [((comp min distances) connections)]))
--- test/adventofcode/day_09_test.clj
+++ test/adventofcode/day_09_test.clj
@@ -1,3 +1,22 @@
 (ns adventofcode.day-09-test
   (:require [clojure.test :refer :all]
             [adventofcode.day-09 :refer :all]))
+
+(deftest parse-connection-test
+  (is (= {#{:alpha :beta} 100}
+         (parse-connection "Alpha to Beta = 100")))
+  (is (= {#{:alpha :beta} 120}
+         (parse-connection "Beta to Alpha = 120"))))
+
+(deftest locations-test
+  (is (= #{:a :b :c}
+         (locations {#{:a :b} 10 #{:a :c} 12 #{:b :c} 15}))))
+
+(deftest distance-test
+  (is (= 22 (distance
+             {#{:a :b} 2 #{:a :c} 7 #{:b :c} 9 #{:c :d} 11}
+             [:a :b :c :d]))))
+
+(deftest distances-test
+  (is (= #{982 605 659}
+         (distances {#{:a :b} 464 #{:a :c} 518 #{:b :c} 141}))))
