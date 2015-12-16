dcce425ef00ca91d33684614d67028b2d26d7cf8
Evan Sosenko <razorx@evansosenko.com>
2015-12-15 21:19:36 -0800

    Solve day 8 part 1

--- src/adventofcode/day_08.clj
+++ src/adventofcode/day_08.clj
@@ -1,6 +1,22 @@
 (ns adventofcode.day-08)
 
+(def matchers
+  {:quote #"\\\""
+   :slash #"\\\\"
+   :xcode #"\\x[0-9a-f]{2}"})
+
+(def matcher-order [:slash :xcode :quote])
+
+(defn in-memory-characters
+  "Returns the number of in-memory characters for a string."
+  [string]
+  (+ -2 (count
+         (reduce #(clojure.string/replace %1 %2 "Z")
+                 string (map matchers matcher-order)))))
+
 (defn solve
   "Given the input for the day, returns the solution."
   [input]
-  "")
+  (let [strings (clojure.string/split-lines input)]
+    [(- (reduce + (map count strings))
+        (reduce + (map in-memory-characters strings)))]))
--- test/adventofcode/day_08_test.clj
+++ test/adventofcode/day_08_test.clj
@@ -1,3 +1,21 @@
 (ns adventofcode.day-08-test
   (:require [clojure.test :refer :all]
             [adventofcode.day-08 :refer :all]))
+
+(deftest matchers-test
+  (is (= "\\\""  (re-find (:quote matchers) "abc\\\"def")))
+  (is (= "\\\\"  (re-find (:slash matchers) "abc\\\\def")))
+  (is (= "\\x89" (re-find (:xcode matchers) "abc\\x89def")))
+  (is (= "\\x7b" (re-find (:xcode matchers) "abc\\x7bdef")))
+  (is (= "\\xaa" (re-find (:xcode matchers) "abc\\xaadef")))
+  (is (= "\\xd6" (re-find (:xcode matchers) "abc\\xd6def"))))
+
+(deftest in-memory-characters-test
+  (is (= 0  (in-memory-characters "\"\"")))
+  (is (= 3  (in-memory-characters "\"abc\"")))
+  (is (= 7  (in-memory-characters "\"aaa\\\"aaa\"")))
+  (is (= 1  (in-memory-characters "\"\\x27\"")))
+  (is (= 1  (in-memory-characters "\"\\\\\"")))
+  (is (= 5  (in-memory-characters "\"\\\"\\\\z9\\x27\"")))
+  (is (= 30 (in-memory-characters
+             "\"\\\"vhzlbwq\\\"xeimjt\\\\xe\\x85umho\\\"m\\\"\\\"bmy\""))))
