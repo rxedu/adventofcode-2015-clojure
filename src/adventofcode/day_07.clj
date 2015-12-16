9371372a0a9a63e1aad168f1d2190c4d06450bef
Evan Sosenko <razorx@evansosenko.com>
2015-12-15 18:57:37 -0800

    Solve day 7 part 1

--- src/adventofcode/day_07.clj
+++ src/adventofcode/day_07.clj
@@ -1,6 +1,82 @@
 (ns adventofcode.day-07)
 
+(def matchers
+  {:sig #"(\d+) -> (\w+)"
+   :con #"(\w+) -> (\w+)"
+   :not #"NOT (\w+) -> (\w+)"
+   :and #"(\w+) AND (\w+) -> (\w+)"
+   :1nd #"1 AND (\w+) -> (\w+)"
+   :or  #"(\w+) OR (\w+) -> (\w+)"
+   :lsh #"(\w+) LSHIFT (\d+) -> (\w+)"
+   :rsh #"(\w+) RSHIFT (\d+) -> (\w+)"})
+
+(def elements
+  {:sig #(hash-map (keyword %2) (vector (Integer. %1)))
+   :con #(hash-map (keyword %2) (vector (keyword %1)))
+   :not #(hash-map (keyword %2) (vector (keyword %1)))
+   :and #(hash-map (keyword %3) (vector (keyword %1) (keyword %2)))
+   :1nd #(hash-map (keyword %2) (vector 1 (keyword %1)))
+   :or  #(hash-map (keyword %3) (vector (keyword %1) (keyword %2)))
+   :lsh #(hash-map (keyword %3) (vector (keyword %1) (Integer. %2)))
+   :rsh #(hash-map (keyword %3) (vector (keyword %1) (Integer. %2)))})
+
+(def gates
+  {:sig identity
+   :con identity
+   :not bit-not
+   :and bit-and
+   :1nd bit-and
+   :or  bit-or
+   :lsh bit-shift-left
+   :rsh bit-shift-right})
+
+(defn parse-connection
+  "Converts a connection string to a vector of the general form:
+  [:type {:output [input(s)]}]."
+  [string]
+  (some (fn [[el matches]]
+          (and matches [el (apply (el elements) (rest matches))]))
+        (reduce
+         (fn [new-map [k v]]
+           (assoc new-map k (re-matches v string)))
+         {} matchers)))
+
+(defn connection-inputs
+  "Given a connection, returns the required inputs as seq of keywords."
+  [connection]
+  (filter keyword? ((comp flatten vals second) connection)))
+
+(defn missing-inputs?
+  "Checks if the provided wire values are missing
+  any inputs required for the given connection."
+  [wires connection]
+  (not-every? identity
+              (map (partial contains? wires) (connection-inputs connection))))
+
+(defn connect
+  "Uses the provided wire values as input to the given connection
+  and returns the new wire values.
+  If any required values are missing, returns the wire values unmodified."
+  [wires connection]
+  (if (missing-inputs? wires connection) wires
+      (let [gate ((first connection) gates)
+            output ((comp first keys second) connection)
+            inputs ((comp first vals second) connection)
+            input-vals (map #(if (keyword? %) (% wires) %) inputs)]
+        (assoc wires output (apply gate input-vals)))))
+
+(defn solve-circuit
+  "Given a seq of connections, returns the stable values of all wires."
+  ([connections]
+   (solve-circuit connections (reduce connect {} connections)))
+  ([connections wires]
+   (let [new-wires (reduce connect wires connections)]
+     (if (= wires new-wires)
+       wires
+       (solve-circuit connections new-wires)))))
+
 (defn solve
   "Given the input for the day, returns the solution."
   [input]
-  "")
+  (let [connections (map parse-connection (clojure.string/split-lines input))]
+    [(:a (solve-circuit connections))]))
--- test/adventofcode/day_07_test.clj
+++ test/adventofcode/day_07_test.clj
@@ -1,3 +1,61 @@
 (ns adventofcode.day-07-test
   (:require [clojure.test :refer :all]
             [adventofcode.day-07 :refer :all]))
+
+(deftest parse-connection-test
+  (is (= [:sig {:xy [42]}]   (parse-connection "42 -> xy")))
+  (is (= [:con {:xx [:zz]}]  (parse-connection "zz -> xx")))
+  (is (= [:not {:zz [:xx]}]  (parse-connection "NOT xx -> zz")))
+  (is (= [:and {:z [:x :y]}] (parse-connection "x AND y -> z")))
+  (is (= [:or  {:z [:x :y]}] (parse-connection "x OR y -> z")))
+  (is (= [:lsh {:z [:x 2]}]  (parse-connection "x LSHIFT 2 -> z")))
+  (is (= [:rsh {:z [:x 2]}]  (parse-connection "x RSHIFT 2 -> z"))))
+
+(deftest connection-inputs-test
+  (is (= []       (connection-inputs [:sig {:xy [42]}])))
+  (is (= [:xx]    (connection-inputs [:not {:zz [:xx]}])))
+  (is (= [:x :yy] (connection-inputs [:and {:z [:x :yy]}])))
+  (is (= [:x]     (connection-inputs [:rsh {:z [:x 5]}]))))
+
+(deftest missing-inputs?-test
+  (is (false? (missing-inputs? {:x 1 :y 2} [:sig {:z [42]}])))
+  (is (false? (missing-inputs? {:x 1 :y 2} [:and {:z [:x :y]}])))
+  (is (true?  (missing-inputs? {:x 1} [:and {:z [:x :y]}]))))
+
+(deftest connect-test
+  (let [wires {:x 3 :y 5 :z 6}]
+    (is (= {:x 3 :y 5 :z 6}        (connect wires [:and {:z [:x :a]}])))
+    (is (= {:x 3 :y 5 :z 42}       (connect wires [:sig {:z [42]}])))
+    (is (= {:x 3 :y 5 :z 6 :a 42}  (connect wires [:sig {:a [42]}])))
+    (is (= {:x 3 :y 5 :z 1}        (connect wires [:and {:z [:x :y]}])))
+    (is (= {:x 3 :y 5 :z -4}       (connect wires [:not {:z [:x]}])))
+    (is (= {:x 3 :y 5 :z 6 :a 320} (connect wires [:lsh {:a [:y :z]}])))))
+
+(deftest solve-circuit-test
+  (let [simple-connections
+        (map parse-connection
+             ["123 -> x"
+              "456 -> y"
+              "x AND y -> a"
+              "x OR y -> e"
+              "x LSHIFT 2 -> f"
+              "y RSHIFT 2 -> g"
+              "NOT x -> h"
+              "NOT y -> i"])
+        two-level-connections
+        (map parse-connection
+             ["z -> x"
+              "NOT z -> aa"
+              "456 -> y"
+              "x AND y -> a"
+              "x OR y -> e"
+              "x LSHIFT 2 -> f"
+              "y RSHIFT 2 -> g"
+              "NOT x -> h"
+              "NOT y -> i"
+              "123 -> z"])
+        simple-wires
+        {:a 72, :e 507, :f 492, :g 114, :h -124, :i -457, :x 123, :y 456}
+        two-level-wires (assoc simple-wires :aa -124 :z 123)]
+    (is (= simple-wires (solve-circuit simple-connections)))
+    (is (= two-level-wires (solve-circuit two-level-connections)))))
