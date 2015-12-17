36dc395383d76216f9f501c2cd7b4df3f3cc779b
Evan Sosenko <razorx@evansosenko.com>
2015-12-16 22:15:01 -0800

    Solve day 11 part 1

--- src/adventofcode/day_11.clj
+++ src/adventofcode/day_11.clj
@@ -1,6 +1,73 @@
 (ns adventofcode.day-11)
 
+(def ** #(reduce * (repeat %2 %1)))
+(def alph (map char (range 97 123)))
+
+(defn alph-triples
+  "Returns a lazy-seq of strings: (abc, bcd, cde,..., xyz)."
+  ([] (alph-triples 0))
+  ([n] (lazy-seq
+        (if (= n 24)
+          nil
+          (cons (apply str (map (partial nth alph) (range n (+ n 3))))
+                (alph-triples (inc n)))))))
+
+(defn valid-password?
+  "Returns true if the string is a valid password, else false."
+  [string]
+  (boolean (and
+            (nil? (re-seq #"[iol]" string))
+            (<= 2 (count (set (re-seq #"(\w)\1" string))))
+            (some #(re-seq (re-pattern %) string) (alph-triples)))))
+
+(defn str->int
+  "Converts a string to its integer representation."
+  [string]
+  (let [base (count alph)
+        idx-alph (into {} (map-indexed #(vector %2 %1) alph))]
+    (apply + (map-indexed
+              #(* (** base %1) (get idx-alph %2))
+              ((comp rseq vec seq) string)))))
+
+(defn int->str
+  "Converts an integer to its string representation."
+  [integer]
+  (if (zero? integer)
+    ((comp str first) alph)
+    (loop [n integer
+           digits ()]
+      (let [b (count alph)]
+        (if (pos? n)
+          (recur (quot n b)
+                 (conj digits (nth alph (mod n b))))
+          (apply str digits))))))
+
+(defn inc-string
+  "Increments a string lexicographically."
+  [string]
+  ((comp int->str inc str->int) string))
+
+(defn pad-string
+  "Pads string to length n by repeatedly prepending a."
+  [n string]
+  (loop [digits (seq string)]
+    (if (<= n (count digits))
+      (apply str digits)
+      (recur (conj digits (first alph))))))
+
+(defn inc-password
+  "Returns the next valid password."
+  [password]
+  (loop [string password]
+    (if (valid-password? string)
+      string
+      (let [next-string (inc-string string)]
+        (recur (pad-string
+                (max (count string) (count next-string))
+                next-string))))))
+
 (defn solve
   "Given the input for the day, returns the solution."
   [input]
-  "")
+  (let [password (clojure.string/trim-newline input)]
+    [(inc-password password)]))
--- test/adventofcode/day_11_test.clj
+++ test/adventofcode/day_11_test.clj
@@ -1,3 +1,71 @@
 (ns adventofcode.day-11-test
   (:require [clojure.test :refer :all]
             [adventofcode.day-11 :refer :all]))
+
+(deftest alph-test
+  (is (= \a (first alph)))
+  (is (= [\a \b \c] (take 3 alph)))
+  (is (= \z (last alph)))
+  (is (= 26 (count alph))))
+
+(deftest alph-triples-test
+  (is (= "abc" (first (alph-triples))))
+  (is (= ["abc" "bcd" "cde"] (take 3 (alph-triples))))
+  (is (= "xyz" (last (alph-triples)))))
+
+(deftest valid-password?-test
+  (is (false? (valid-password? "hijklmmn")))
+  (is (false? (valid-password? "abbceffg")))
+  (is (false? (valid-password? "abbcegjk")))
+  (is (false? (valid-password? "ghijklmn")))
+  (is (false? (valid-password? "abcdefgh")))
+  (is (false? (valid-password? "ghijklmn")))
+  (is (false? (valid-password? "affceffg")))
+  (is (true?  (valid-password? "abcdffaa")))
+  (is (true?  (valid-password? "ghjaabcc"))))
+
+(deftest int->str-test
+  (is (= "a"        (int->str 0)))
+  (is (= "b"        (int->str 1)))
+  (is (= "z"        (int->str 25)))
+  (is (= "ba"       (int->str 26)))
+  (is (= "zz"       (int->str 675)))
+  (is (= "baa"      (int->str 676)))
+  (is (= "sle"      (int->str 12458)))
+  (is (= "lsii"     (int->str 205720)))
+  (is (= "sjeughrm" (int->str 147409600818))))
+
+(deftest str->int-test
+  (is (= 0            (str->int "a")))
+  (is (= 1            (str->int "b")))
+  (is (= 25           (str->int "z")))
+  (is (= 26           (str->int "ba")))
+  (is (= 675          (str->int "zz")))
+  (is (= 676          (str->int "baa")))
+  (is (= 12458        (str->int "sle")))
+  (is (= 205720       (str->int "lsii")))
+  (is (= 147409600818 (str->int "sjeughrm"))))
+
+(deftest inc-string-test
+  (is (= "b"      (inc-string "a")))
+  (is (= "z"      (inc-string "y")))
+  (is (= "ba"     (inc-string "z")))
+  (is (= "xy"     (inc-string "xx")))
+  (is (= "ya"     (inc-string "xz")))
+  (is (= "bjskek" (inc-string "bjskej")))
+  (is (= "kdlsja" (inc-string "kdlsiz")))
+  (is (= "sjeaa"  (inc-string "sjdzz")))
+  (is (= "baaaa"  (inc-string "zzzz"))))
+
+(deftest pad-string-test
+  (is (= "a"          (pad-string 1 "a")))
+  (is (= "b"          (pad-string 1 "b")))
+  (is (= "aaa"        (pad-string 3 "a")))
+  (is (= "aab"        (pad-string 3 "b")))
+  (is (= "aaadusjdh"  (pad-string 9 "dusjdh")))
+  (is (= "hytgf"      (pad-string 5 "hytgf"))))
+
+(deftest inc-password-test
+  (is (= "bbcdd"    (inc-password "bbcbb")))
+  (is (= "abcdffaa" (inc-password "abcdfeaa")))
+  (is (= "ghjaabcc" (inc-password "ghjaabbb"))))
