03b405b08095c6207aa6ec34cd0c346b7e54e900
Evan Sosenko <razorx@evansosenko.com>
2015-12-16 19:37:27 -0800

    Solve day 10 part 1

--- src/adventofcode/day_10.clj
+++ src/adventofcode/day_10.clj
@@ -1,6 +1,19 @@
 (ns adventofcode.day-10)
 
+(defn look-and-say
+  "Returns the look-and-say lazy-seq starting with n (or 1 by default)."
+  ([] (look-and-say 1))
+  ([n] (let [n (str n)]
+         (cons n (look-and-say n n))))
+  ([n, _] (let [digits (seq (str n))
+                groups (partition-by identity digits)
+                looked-and-said
+                (map #(apply str (vector (count %) (first %))) groups)
+                nxt (apply str looked-and-said)]
+            (lazy-seq (cons nxt (look-and-say nxt _))))))
+
 (defn solve
   "Given the input for the day, returns the solution."
   [input]
-  "")
+  (let [n (Integer. (clojure.string/trim-newline input))]
+    [((comp count last) (take 41 (look-and-say n)))]))
--- test/adventofcode/day_10_test.clj
+++ test/adventofcode/day_10_test.clj
@@ -1,3 +1,11 @@
 (ns adventofcode.day-10-test
   (:require [clojure.test :refer :all]
             [adventofcode.day-10 :refer :all]))
+
+(deftest look-and-say-test
+  (is (= ["1" "11" "21" "1211" "111221" "312211" "13112221" "1113213211"]
+         (take 8 (look-and-say))))
+  (is (= ["2" "12" "1112" "3112" "132112" "1113122112" "311311222112"]
+         (take 7 (look-and-say 2))))
+  (is (= ["3" "13" "1113" "3113" "132113" "1113122113" "311311222113"]
+         (take 7 (look-and-say 3)))))
