f0c4597825ba579cfb5a9957e689cf69eda4e1cc
Evan Sosenko <razorx@evansosenko.com>
2015-12-17 17:55:35 -0800

    Solve day 12 part 1

--- src/adventofcode/day_12.clj
+++ src/adventofcode/day_12.clj
@@ -1,6 +1,21 @@
 (ns adventofcode.day-12)
 
+(def parse-int #(Integer/parseInt %))
+
+(def matchers
+  [#"\[(-?\d+)\]"
+   #",(-?\d+)\]"
+   #"(-?\d+),"
+   #":(-?\d+)}"])
+
+(defn sum-numbers
+  "Sums all numbers in a JSON formatted string."
+  [string]
+  (reduce + (map
+             #(apply + (map (comp parse-int second) (re-seq % string)))
+             matchers)))
+
 (defn solve
   "Given the input for the day, returns the solution."
   [input]
-  "")
+  [(sum-numbers input)])
--- test/adventofcode/day_12_test.clj
+++ test/adventofcode/day_12_test.clj
@@ -1,3 +1,17 @@
 (ns adventofcode.day-12-test
   (:require [clojure.test :refer :all]
             [adventofcode.day-12 :refer :all]))
+
+(deftest sum-numbers-test
+  (is (= 6 (sum-numbers "[1,2,3]")))
+  (is (= 6 (sum-numbers "{\"a\":2,\"b\":4}")))
+  (is (= 3 (sum-numbers "[[[3]]]")))
+  (is (= 3 (sum-numbers "{\"a\":{\"b\":4},\"c\":-1}")))
+  (is (= 0 (sum-numbers "{\"a\":[-1,1]}")))
+  (is (= 0 (sum-numbers "[-1,{\"a\":1}]")))
+  (is (= 0 (sum-numbers "[]")))
+  (is (= 0 (sum-numbers "{}")))
+  (is (= 608
+         (sum-numbers
+          "{\"e\":57,\"c\":\"red\",\"a\":167,\"b\":[-42,147,166,74,-32,
+          \"orange\",\"violet\",\"yellow\"],\"d\":\"green\",\"f\":71}}"))))
