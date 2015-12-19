387043619068e13eaa963237c907f47f81bf1316
Evan Sosenko <razorx@evansosenko.com>
2015-12-18 17:22:00 -0800

    Solve day 13 part 1

--- src/adventofcode/day_13.clj
+++ src/adventofcode/day_13.clj
@@ -1,6 +1,48 @@
-(ns adventofcode.day-13)
+(ns adventofcode.day-13
+  (:require [clojure.math.combinatorics]))
+
+(defn parse-relation
+  "Converts a happiness relation from a string
+  to the form {{:alice :bob} happiness},
+  e.g., Alice would gain 100 happiness units by sitting next to Bob
+  becomes {{:alice :bob} 100}."
+  [string]
+  (let [matcher
+        #"(\w+) would (\w+) (\d+) happiness units by sitting next to (\w+)\."
+        [a sign value b]
+        (rest (re-matches matcher string))
+        signed-value
+        (if (= sign "gain") (Integer. value) (- 0 (Integer. value)))]
+    (assoc {} (apply array-map
+                     (map (comp keyword clojure.string/lower-case) [a b]))
+           signed-value)))
+
+(defn guests
+  "Given a map of relations, returns a set of guests."
+  [relations]
+  ((comp set flatten) (apply concat (keys relations))))
+
+(defn happiness
+  "Given a map of relations,
+  returns the total happiness for a seating arrangement"
+  [relations seating]
+  (let [table (concat seating [(first seating)] ((comp reverse) seating))]
+    (first (reduce (fn [[dist pre] nxt]
+                     [(+ dist (relations (apply array-map [pre nxt]))) nxt])
+                   [0 (first table)] (rest table)))))
+
+(defn potential-happiness
+  "Given a seq of relations, returns the set of potential happiness
+  for all seating arrangements."
+  [relations]
+  (set (map (partial happiness relations)
+            (clojure.math.combinatorics/permutations
+             (guests relations)))))
 
 (defn solve
   "Given the input for the day, returns the solution."
   [input]
-  "")
+  (let [relations
+        (into {} (map parse-relation
+                      (clojure.string/split-lines input)))]
+    [(apply max (potential-happiness relations))]))
--- test/adventofcode/day_13_test.clj
+++ test/adventofcode/day_13_test.clj
@@ -1,3 +1,46 @@
 (ns adventofcode.day-13-test
   (:require [clojure.test :refer :all]
             [adventofcode.day-13 :refer :all]))
+
+(deftest parse-relation-test
+  (is (= {{:alice :bob} 42}
+         (parse-relation
+          "Alice would gain 42 happiness units by sitting next to Bob.")))
+  (is (= {{:bob :alice} 50}
+         (parse-relation
+          "Bob would gain 50 happiness units by sitting next to Alice.")))
+  (is (= {{:bob :alice} -20}
+         (parse-relation
+          "Bob would loose 20 happiness units by sitting next to Alice."))))
+
+(deftest guests-test
+  (is (= #{:a :b :c}
+         (guests
+          {{:a :b} 1 {:a :c} -2 {:b :c} 3 {:b :a} 3 {:c :a} 1 {:c :b} 4}))))
+
+(deftest happiness-test
+  (is (= -18 (happiness
+              {{:a :b} 10 {:a :c} -20 {:b :c} 32 {:b :a} -44 {:c :a} 8 {:c :b} -4}
+              [:b :a :c]))))
+
+(deftest potential-happiness-test
+  (is (= #{2}
+         (potential-happiness
+          {{:a :b} 1 {:a :c} -2 {:b :c} 3 {:b :a} 3 {:c :a} 1 {:c :b} -4}))))
+
+(deftest solve-test
+  (let [input
+        "Alice would gain 54 happiness units by sitting next to Bob.
+Alice would lose 79 happiness units by sitting next to Carol.
+Alice would lose 2 happiness units by sitting next to David.
+Bob would gain 83 happiness units by sitting next to Alice.
+Bob would lose 7 happiness units by sitting next to Carol.
+Bob would lose 63 happiness units by sitting next to David.
+Carol would lose 62 happiness units by sitting next to Alice.
+Carol would gain 60 happiness units by sitting next to Bob.
+Carol would gain 55 happiness units by sitting next to David.
+David would gain 46 happiness units by sitting next to Alice.
+David would lose 7 happiness units by sitting next to Bob.
+David would gain 41 happiness units by sitting next to Carol."]
+    (testing "part 1"
+      (is (= 330 (first (solve input)))))))
