3e873f284f14a3ea059f1a2b9a9f0ac1a73b2257
Evan Sosenko <razorx@evansosenko.com>
2015-12-18 22:27:44 -0800

    Solve day 14 part 1

--- src/adventofcode/day_14.clj
+++ src/adventofcode/day_14.clj
@@ -1,6 +1,33 @@
 (ns adventofcode.day-14)
 
+(defn parse-reindeer
+  "Converts a reindeer string to a map, e.g.,
+  {:rudolph {:speed 10 :fly 15 :rest 5}}."
+  [string]
+  (let [matches
+        (rest (re-matches #"(\w+)\D+(\d+)\D+(\d+)\D+(\d+)\D+\." string))]
+    (assoc {}
+           ((comp keyword clojure.string/lower-case) (first matches))
+           (into {} (map hash-map
+                         [:speed :fly :rest]
+                         (map #(Integer/parseInt %) (rest matches)))))))
+
+(defn flying?
+  "Returns if the reindeer is flying after t seconds."
+  [reindeer t]
+  (< 0 (mod t (apply + (map reindeer [:fly :rest])))
+     (inc (:fly reindeer))))
+
+(defn distance
+  "Returns to the total distance a reindeer has traveled after t seconds."
+  [reindeer t]
+  (reduce
+   #(if (flying? reindeer %2) (+ %1 (:speed reindeer)) %1)
+   0 (range (inc t))))
+
 (defn solve
   "Given the input for the day, returns the solution."
   [input]
-  "")
+  (let [reindeer
+        (into {} (map parse-reindeer (clojure.string/split-lines input)))]
+    [(apply max (map #(distance % 2503) (vals reindeer)))]))
--- test/adventofcode/day_14_test.clj
+++ test/adventofcode/day_14_test.clj
@@ -1,3 +1,51 @@
 (ns adventofcode.day-14-test
   (:require [clojure.test :refer :all]
             [adventofcode.day-14 :refer :all]))
+
+(deftest parse-reindeer-test
+  (is (= {:comet {:speed 14 :fly 10 :rest 127}}
+         (parse-reindeer
+          "Comet can fly 14 km/s for 10 seconds,
+          but then must rest for 127 seconds.")))
+  (is (= {:dancer {:speed 16 :fly 11 :rest 162}}
+         (parse-reindeer
+          "Dancer can fly 16 km/s for 11 seconds,
+          but then must rest for 162 seconds."))))
+
+(deftest flying?-test
+  (let [comet {:speed 14 :fly 10 :rest 127}
+        vixen {:speed 16 :fly 11 :rest 162}]
+    (are [x r t] (x (flying? r t))
+      false? comet 0
+      false? vixen 0
+      true?  comet 1
+      true?  vixen 1
+      true?  comet 10
+      true?  vixen 11
+      false? comet 11
+      false? vixen 12
+      false? comet 127
+      false? vixen 162
+      false? comet 128
+      false? vixen 163
+      false? comet 137
+      false? vixen 173
+      true?  comet 138
+      true?  vixen 174)))
+
+(deftest distance-test
+  (let [comet {:speed 14 :fly 10 :rest 127}
+        vixen {:speed 16 :fly 11 :rest 162}]
+    (are [x r t] (= x (distance r t))
+      0    comet 0
+      0    vixen 0
+      14   comet 1
+      16   vixen 1
+      140  comet 10
+      160  vixen 10
+      140  comet 11
+      176  vixen 11
+      140  comet 137
+      176  vixen 173
+      1120 comet 1000
+      1056 vixen 1000)))
