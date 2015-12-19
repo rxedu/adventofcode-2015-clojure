dc18a5b0f1e843e042166e6fb77f14ece1091b57
Evan Sosenko <razorx@evansosenko.com>
2015-12-19 15:03:09 -0800

    Solve day 15 part 1

--- src/adventofcode/day_15.clj
+++ src/adventofcode/day_15.clj
@@ -1,6 +1,52 @@
-(ns adventofcode.day-15)
+(ns adventofcode.day-15
+  (:require [clojure.math.combinatorics]))
+
+(defn integer-partitions
+  "Returns a seq of all the ways to write the integer x
+  as a sum of n non-negative integers."
+  [x n]
+  (filter #(= x (apply + %))
+          (clojure.math.combinatorics/selections (range (inc x)) n)))
+
+(defn parse-ingredient
+  "Converts an ingredient string into a map,
+  e.g., {:butterscotch {:capacity 5, :durability 2}}."
+  [string]
+  (let [ingredient ((comp second first)
+                    (re-seq #"^(\w+):" string))
+        properties (map (comp #(take 2 %) rest)
+                        (re-seq #"(\w+) (-?\d+)" string))
+        format-fn #(vector (keyword (first %)) (Integer. (second %)))]
+    {((comp keyword clojure.string/lower-case) ingredient)
+     (apply array-map (flatten (map format-fn properties)))}))
+
+(defn cookie
+  "Given a list of ingredients and the amounts of each,
+  returns the totaled values of all properties."
+  [ingredients amounts]
+  ((comp #(apply (partial merge-with +) %) vals)
+   (reduce
+    (fn [scores amount]
+      (let [ingredient (first amount)
+            quantity (second amount)
+            properties (ingredient ingredients)]
+        (assoc scores
+               ingredient
+               (zipmap (keys properties)
+                       (map #(* % quantity) (vals properties))))))
+    {} amounts)))
+
+(defn score
+  "Given a list of a cookie's properties, returns the total cookie score."
+  [cookie]
+  (reduce #(* %1 (max 0 %2)) 1 (vals (dissoc cookie :calories))))
 
 (defn solve
   "Given the input for the day, returns the solution."
   [input]
-  "")
+  (let [ingredients
+        (into {} (map parse-ingredient (clojure.string/split-lines input)))
+        types (keys ingredients)]
+    [(reduce (fn [best amounts]
+               (max best (score (cookie ingredients (zipmap types amounts)))))
+             0 (integer-partitions 100 (count types)))]))
--- test/adventofcode/day_15_test.clj
+++ test/adventofcode/day_15_test.clj
@@ -1,3 +1,38 @@
 (ns adventofcode.day-15-test
   (:require [clojure.test :refer :all]
             [adventofcode.day-15 :refer :all]))
+
+(deftest integer-partitions-test
+  (is (= [[0 0 2] [0 1 1] [0 2 0] [1 0 1] [1 1 0] [2 0 0]]
+         (integer-partitions 2 3))))
+
+(deftest parse-ingredient-test
+  (is (= {:butterscotch
+          {:capacity -1 :durability -2 :flavor 6 :texture 3 :calories 8}}
+         (parse-ingredient "Butterscotch: capacity -1, durability -2,
+                           flavor 6, texture 3, calories 8"))))
+
+(deftest cookie-test
+  (let [ingredients
+        {:butterscotch {:calories 8, :flavor  6, :texture  3}
+         :cinnamon     {:calories 3, :flavor -2, :texture -1}}]
+    (is (= {:calories 16, :flavor 12, :texture 6}
+           (cookie ingredients {:butterscotch 2})))
+    (is (= {:calories 11, :flavor 4, :texture 2}
+           (cookie ingredients {:butterscotch 1 :cinnamon 1})))
+    (is (= {:calories 25, :flavor 6, :texture 3}
+           (cookie ingredients {:butterscotch 2 :cinnamon 3})))))
+
+(deftest score-test
+  (let
+   [ingredients
+    {:butterscotch
+     {:calories 8, :capacity -1, :durability -2, :flavor  6, :texture  3}
+     :cinnamon
+     {:calories 3, :capacity  2, :durability  3, :flavor -2, :texture -1}}]
+    (are [s a] (= s (score (cookie ingredients a)))
+      0        {:butterscotch 1}
+      0        {:butterscotch 0  :cinnamon 1}
+      0        {:butterscotch 10 :cinnamon 1}
+      8        {:butterscotch 1  :cinnamon 1}
+      62842880 {:butterscotch 44 :cinnamon 56})))
