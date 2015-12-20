1eee13867067c2d000c2efaea655c6404b480d2d
Evan Sosenko <razorx@evansosenko.com>
2015-12-19 16:05:12 -0800

    Solve day 16 part 1

--- src/adventofcode/day_16.clj
+++ src/adventofcode/day_16.clj
@@ -1,6 +1,41 @@
 (ns adventofcode.day-16)
 
+(def suspect
+  {:children 3
+   :cats 7
+   :samoyeds 2
+   :pomeranians 3
+   :akitas 0
+   :vizslas 0
+   :goldfish 5
+   :trees 3
+   :cars 2
+   :perfumes 1})
+
+(defn parse-sue
+  "Converts a string for an Aunt Sue to a map,
+  e.g., {1 {:cats 5, :cars 3, :trees 5}}."
+  [string]
+  (let [sue ((comp second first)
+             (re-seq #"^Sue (\d+):" string))
+        attributes (map (comp #(take 2 %) rest)
+                        (re-seq #"(\w+): (\d+)" string))
+        format-fn #(vector (keyword (first %)) (Integer. (second %)))]
+    {(Integer. sue)
+     (apply array-map (flatten (map format-fn attributes)))}))
+
+(defn compare-sue
+  "Given the required properties for the target Sue,
+  returns whether the input Sue could be a match."
+  [target sue]
+  (not-any? false? (vals (merge-with = target sue))))
+
 (defn solve
   "Given the input for the day, returns the solution."
   [input]
-  "")
+  (let [aunts
+        (into {} (map parse-sue (clojure.string/split-lines input)))]
+    [((comp first keys)
+      (filter
+       #(true? (compare-sue suspect (second %)))
+       aunts))]))
--- test/adventofcode/day_16_test.clj
+++ test/adventofcode/day_16_test.clj
@@ -1,3 +1,14 @@
 (ns adventofcode.day-16-test
   (:require [clojure.test :refer :all]
             [adventofcode.day-16 :refer :all]))
+
+(deftest parse-sue-test
+  (is (= {1 {:cars 9 :akitas 3 :goldfish 0}}
+         (parse-sue "Sue 1: cars: 9, akitas: 3, goldfish: 0"))))
+
+(deftest compare-sue-test
+  (is (false? (compare-sue {:a 1 :b 2 :c 3} {:a 1 :b 2 :c 5})))
+  (is (false? (compare-sue {:a 1 :b 2 :c 3} {:a 1 :b 1 :c 5})))
+  (is (true?  (compare-sue {:a 1 :b 2 :c 3} {:a 1 :b 2 :c 3})))
+  (is (true?  (compare-sue {:a 1 :b 2 :c 3} {:a 1 :b 2 :d 3})))
+  (is (true?  (compare-sue {:a 1 :b 2 :d 3} {:a 1 :b 2 :c 3}))))
