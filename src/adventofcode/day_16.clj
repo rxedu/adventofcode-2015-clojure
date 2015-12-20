b153fd78067f63e4a95f8ce1ab54e4aa5b508356
Evan Sosenko <razorx@evansosenko.com>
2015-12-19 16:55:43 -0800

    Solve day 16 part 2

--- src/adventofcode/day_16.clj
+++ src/adventofcode/day_16.clj
@@ -30,12 +30,24 @@
   [target sue]
   (not-any? false? (vals (merge-with = target sue))))
 
+(defn compare-real-sue
+  "Given the required properties for the target Sue,
+  returns whether the input Sue could be a real match."
+  [target sue]
+  (let [gt [:cats :trees]
+        lt [:pomeranians :goldfish]
+        fn-gt #(select-keys % gt)
+        fn-lt #(select-keys % lt)
+        fn-eq #(apply (partial dissoc %) (concat gt lt))]
+    ((comp (partial not-any? false?) flatten)
+     (map vals [(merge-with = (fn-eq target) (fn-eq sue))
+                (merge-with < (fn-gt target) (fn-gt sue))
+                (merge-with > (fn-lt target) (fn-lt sue))]))))
+
 (defn solve
   "Given the input for the day, returns the solution."
   [input]
-  (let [aunts
-        (into {} (map parse-sue (clojure.string/split-lines input)))]
-    [((comp first keys)
-      (filter
-       #(true? (compare-sue suspect (second %)))
-       aunts))]))
+  (let [aunts (into {} (map parse-sue (clojure.string/split-lines input)))
+        finder-fn #((comp first keys) (filter % aunts))]
+    [(finder-fn #(true? (compare-sue suspect (second %))))
+     (finder-fn #(true? (compare-real-sue suspect (second %))))]))
--- test/adventofcode/day_16_test.clj
+++ test/adventofcode/day_16_test.clj
@@ -12,3 +12,15 @@
   (is (true?  (compare-sue {:a 1 :b 2 :c 3} {:a 1 :b 2 :c 3})))
   (is (true?  (compare-sue {:a 1 :b 2 :c 3} {:a 1 :b 2 :d 3})))
   (is (true?  (compare-sue {:a 1 :b 2 :d 3} {:a 1 :b 2 :c 3}))))
+
+(deftest compare-sue-test
+  (is (false? (compare-real-sue {:goldfish 4 :c 3} {:goldfish 5 :c 3})))
+  (is (false? (compare-real-sue {:goldfish 4 :c 3} {:goldfish 4 :c 3})))
+  (is (true?  (compare-real-sue {:goldfish 4 :c 3} {:goldfish 3 :c 3})))
+  (is (true?  (compare-real-sue {:cats 4 :c 3} {:cats 5 :c 3})))
+  (is (false? (compare-real-sue {:cats 4 :c 3} {:cats 4 :c 3})))
+  (is (false? (compare-real-sue {:cats 4 :c 3} {:cats 3 :c 3})))
+  (is (false? (compare-real-sue {:a 1 :b 2 :c 3} {:a 1 :b 1 :c 5})))
+  (is (true?  (compare-real-sue {:a 1 :b 2 :c 3} {:a 1 :b 2 :c 3})))
+  (is (true?  (compare-real-sue {:cats 4 :goldfish 8 :d 3}
+                                {:cats 6 :goldfish 2 :d 3 :e 4}))))
