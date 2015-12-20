3d568aed9f7cef27729940179cb948854a4dbb44
Evan Sosenko <razorx@evansosenko.com>
2015-12-19 23:53:26 -0800

    Solve day 18 part 1

--- src/adventofcode/day_18.clj
+++ src/adventofcode/day_18.clj
@@ -1,6 +1,61 @@
-(ns adventofcode.day-18)
+(ns adventofcode.day-18
+  (:require [clojure.math.combinatorics]))
+
+(defn parse-lights
+  "Converts a row of lights to a seq of boolean values,
+  e.g., .#.## becomes [false true false true true]."
+  [string]
+  (replace {\. false \# true} (seq string)))
+
+(defn nearest-neighbors
+  "Returns the nearest neighbors of a point [x y] in an n-by-n grid."
+  [n point]
+  (let [neighbor-fns
+        (rest (clojure.math.combinatorics/selections [identity inc dec] 2))]
+    (filter #(and (<= 0 (first %) (dec n)) (<= 0 (second %) (dec n)))
+            (map #(map (fn [f p] (f p)) %1 %2)
+                 neighbor-fns (repeat point)))))
+
+(def nearest-neighbors-memo (memoize nearest-neighbors))
+
+(defn neighbor-status
+  "Given a grid and a point, returns a map with the number
+  of nearest neighbors which are on and off, e.g., {true 3, false 5}"
+  [grid point]
+  (let [neighbors (nearest-neighbors-memo (count grid) point)]
+    (frequencies
+     (map (fn [[x y]] (nth (nth grid x) y)) neighbors))))
+
+(defn animate-light
+  "Returns the new value of the light at a point on the grid."
+  [grid [x y :as point]]
+  (let [status (nth (nth grid x) y)
+        neighbors-on (get (neighbor-status grid point) true 0)]
+    (if (or (and (true? status) (< 1 neighbors-on 4))
+            (and (false? status) (= 3 neighbors-on)))
+      true false)))
+
+(defn step-grid
+  "Returns a new grid where the value at each point
+  is determined by the value of the step function
+  applied to the point and the input grid."
+  [step-fn grid]
+  (let [grid-size (range (count grid))]
+    (persistent!
+     (reduce
+      (fn [row x]
+        (conj! row (persistent!
+                    (reduce
+                     (fn [column y]
+                       (conj! column (step-fn grid [x y])))
+                     (transient []) grid-size))))
+      (transient []) grid-size))))
 
 (defn solve
   "Given the input for the day, returns the solution."
   [input]
-  [])
+  (let [grid (map parse-lights
+                  (clojure.string/split-lines input))]
+    [((comp #(get % true) frequencies flatten)
+      (reduce #(step-grid animate-light %1 #_%2)
+              grid (range 100)))]))
--- test/adventofcode/day_18_test.clj
+++ test/adventofcode/day_18_test.clj
@@ -1,3 +1,51 @@
 (ns adventofcode.day-18-test
   (:require [clojure.test :refer :all]
             [adventofcode.day-18 :refer :all]))
+
+(deftest parse-lights-test
+  (is (= [true]   (parse-lights "#")))
+  (is (= [false]  (parse-lights ".")))
+  (is (= [true false true true true false false]
+         (parse-lights "#.###.."))))
+
+(deftest nearest-neighbors-test
+  (is (= [[0 1] [1 0] [1 1]] (nearest-neighbors 10 [0 0])))
+  (is (= [[9 8] [8 9] [8 8]] (nearest-neighbors 10 [9 9])))
+  (is (= [[5 8] [6 9] [6 8] [4 9] [4 8]]
+         (nearest-neighbors 10 [5 9])))
+  (is (= [[4 6] [4 4] [5 5] [5 6] [5 4] [3 5] [3 6] [3 4]]
+         (nearest-neighbors 10 [4 5]))))
+
+(deftest neighbor-status-test
+  (is (= {true 3 false 5}
+         (neighbor-status [[true  true  true  true]
+                           [false false true  true]
+                           [true  false false true]
+                           [false true  false true]] [2 1])))
+  (is (= {true 2 false 1}
+         (neighbor-status [[true  true  true  false]
+                           [false false false true]
+                           [true  false false true]
+                           [false true  false true]] [0 3]))))
+
+(deftest animate-light-test
+  (let [grid [[true  true  true  false]
+              [false false false true]
+              [true  false false false]
+              [false true  false true]]]
+    (is (true?  (animate-light grid [0 1])))
+    (is (true?  (animate-light grid [1 0])))
+    (is (false? (animate-light grid [2 1])))
+    (is (false? (animate-light grid [1 3])))
+    (is (false? (animate-light grid [3 3])))))
+
+(deftest step-grid-test
+  (is (= [[false true  true  false]
+          [true  false false true]
+          [false false false true]
+          [false false true  false]]
+         (step-grid animate-light
+                    [[true  true  true  false]
+                     [false false false true]
+                     [true  false false true]
+                     [false true  false true]]))))
