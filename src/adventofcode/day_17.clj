e49f86fe54720a05e22a435bef58973f1663c63b
Evan Sosenko <razorx@evansosenko.com>
2015-12-19 17:59:16 -0800

    Solve day 17 part 2

--- src/adventofcode/day_17.clj
+++ src/adventofcode/day_17.clj
@@ -3,16 +3,25 @@
 
 (defn container-combinations
   "Given a seq of available container sizes,
-  returns the total number of ways to store the quantity x."
+  returns a seq of possible ways to store the quantity x."
   [sizes x]
-  (count (filter
-          #(= x (reduce + (map second %)))
+  (filter #(= x (reduce + (map second %)))
           (clojure.math.combinatorics/subsets
-           (map-indexed #(vector %1 %2) sizes)))))
+           (map-indexed #(vector %1 %2) sizes))))
+
+(defn min-container-combinations
+  "Given a seq of possible container combinations,
+  returns the number those combinations which use
+  the fewest number of containers."
+  [combinations]
+  (let [num-ways (frequencies (map count combinations))]
+    (get num-ways (apply min (keys num-ways)))))
 
 (defn solve
   "Given the input for the day, returns the solution."
   [input]
   (let [sizes (map #(Integer/parseInt %)
-                   (clojure.string/split-lines input))]
-    [(container-combinations sizes 150)]))
+                   (clojure.string/split-lines input))
+        combinations (container-combinations sizes 150)]
+    [(count combinations)
+     (min-container-combinations combinations)]))
--- test/adventofcode/day_17_test.clj
+++ test/adventofcode/day_17_test.clj
@@ -3,4 +3,8 @@
             [adventofcode.day-17 :refer :all]))
 
 (deftest container-combinations-test
-  (is (= 4 (container-combinations [20 15 10 5 5] 25))))
+  (is (= 4 (count (container-combinations [20 15 10 5 5] 25)))))
+
+(deftest min-container-combinations-test
+  (is (= 3 (min-container-combinations
+            (container-combinations [20 15 10 5 5] 25)))))
