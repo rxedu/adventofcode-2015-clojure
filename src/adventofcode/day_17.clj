28ee914ef51649ed15cf5277bbc6220d145f81fd
Evan Sosenko <razorx@evansosenko.com>
2015-12-19 17:51:23 -0800

    Solve day 17 part 1

--- src/adventofcode/day_17.clj
+++ src/adventofcode/day_17.clj
@@ -1,6 +1,18 @@
-(ns adventofcode.day-17)
+(ns adventofcode.day-17
+  (:require [clojure.math.combinatorics]))
+
+(defn container-combinations
+  "Given a seq of available container sizes,
+  returns the total number of ways to store the quantity x."
+  [sizes x]
+  (count (filter
+          #(= x (reduce + (map second %)))
+          (clojure.math.combinatorics/subsets
+           (map-indexed #(vector %1 %2) sizes)))))
 
 (defn solve
   "Given the input for the day, returns the solution."
   [input]
-  [])
+  (let [sizes (map #(Integer/parseInt %)
+                   (clojure.string/split-lines input))]
+    [(container-combinations sizes 150)]))
--- test/adventofcode/day_17_test.clj
+++ test/adventofcode/day_17_test.clj
@@ -1,3 +1,6 @@
 (ns adventofcode.day-17-test
   (:require [clojure.test :refer :all]
             [adventofcode.day-17 :refer :all]))
+
+(deftest container-combinations-test
+  (is (= 4 (container-combinations [20 15 10 5 5] 25))))
