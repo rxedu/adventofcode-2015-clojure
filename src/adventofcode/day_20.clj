97fffaa68aeaf644a01b508ce01b782552ebca87
Evan Sosenko <razorx@evansosenko.com>
2015-12-21 14:46:36 -0800

    Solving day 20 part 1

--- src/adventofcode/day_20.clj
+++ src/adventofcode/day_20.clj
@@ -1,6 +1,40 @@
-(ns adventofcode.day-20)
+(ns adventofcode.day-20
+  (:require [adventofcode.day-04]))
+
+(defn elf-houses
+  "Returns a lazy seq of houses visited by the nth elf."
+  [n]
+  (take-nth n (drop (dec n) (adventofcode.day-04/positive-numbers))))
+
+(defn visited?
+  "Returns true if house i is visited by elf n, else false."
+  [i n]
+  (zero? (mod i n)))
+
+(def visited?-memo (memoize visited?))
+(def presents-memo
+  (memoize #(if (visited?-memo %1 %3) (+ %2 %3) %2)))
+
+; (defn house-presents
+;   "Returns a lazy seq of the number of presents
+;   delivered to each house."
+;   ([] (house-presents 1))
+;   ([n] (lazy-seq
+;         (cons (reduce (partial presents-memo n) 0 (range 1 (inc n)))
+;               (house-presents (inc n))))))
+
+(defn house-presents
+  "Returns the number of presents delivered to the nth house."
+  [n]
+  (reduce (partial presents-memo n) 0 (range 1 (inc n))))
 
 (defn solve
   "Given the input for the day, returns the solution."
   [input]
-  [])
+  (let [presents
+        (/ (Integer. (clojure.string/trim-newline input)) 10)]
+    [(loop [n 1]
+       (let [total-house-presents (house-presents n)]
+         (if (< total-house-presents presents)
+           (recur (inc n))
+           n)))]))
--- test/adventofcode/day_20_test.clj
+++ test/adventofcode/day_20_test.clj
@@ -1,3 +1,27 @@
 (ns adventofcode.day-20-test
   (:require [clojure.test :refer :all]
             [adventofcode.day-20 :refer :all]))
+
+(deftest elf-houses-test
+  (is (= [1 2 3 4 5]   (take 5 (elf-houses 1))))
+  (is (= [2 4 6 8 10]  (take 5 (elf-houses 2))))
+  (is (= [3 6 9 12 15] (take 5 (elf-houses 3)))))
+
+(deftest visited?-test
+  (are [b i n] (is (b (visited? i n)))
+    true?  1  1
+    true?  7  1
+    false? 1  2
+    true?  2  2
+    true?  18 2
+    false? 7  3
+    true?  10 5))
+
+(deftest house-presents-test
+  (is (= [1 3 4 7 6 12 8 15 13]
+         (map house-presents (range 1 10)))))
+
+(deftest solve-test
+  (testing "part 1"
+    (is (= 8 (first (solve "125"))))
+    (is (= 48 (first (solve "1000"))))))
