deb2d70e23271b6d23c28c089b5df75cc408950e
Evan Sosenko <razorx@evansosenko.com>
2015-12-21 14:46:36 -0800

    Solve day 20 part 1

--- src/adventofcode/day_20.clj
+++ src/adventofcode/day_20.clj
@@ -1,6 +1,20 @@
 (ns adventofcode.day-20)
 
+(set! *unchecked-math* true)
+
+(defn house-presents
+  "Returns the number of presents delivered to the nth house."
+  [n]
+  (let [presents-fn #(if (zero? ^int (mod n %2))
+                       (+ ^int %1 ^int %2) %1)]
+    (* 10 ^int (reduce presents-fn 0 (range 1 (inc ^int n))))))
+
 (defn solve
   "Given the input for the day, returns the solution."
   [input]
-  [])
+  (let [presents (Integer. (clojure.string/trim-newline input))]
+    [(loop [n 1]
+       (let [total-house-presents (house-presents n)]
+         (if (< ^int total-house-presents ^int presents)
+           (recur (inc n))
+           n)))]))
--- test/adventofcode/day_20_test.clj
+++ test/adventofcode/day_20_test.clj
@@ -1,3 +1,12 @@
 (ns adventofcode.day-20-test
   (:require [clojure.test :refer :all]
             [adventofcode.day-20 :refer :all]))
+
+(deftest house-presents-test
+  (is (= [10 30 40 70 60 120 80 150 130]
+         (map house-presents (range 1 10)))))
+
+(deftest solve-test
+  (testing "part 1"
+    (is (= 8 (first (solve "130"))))
+    (is (= 48 (first (solve "1000"))))))
