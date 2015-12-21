662add5cec11c732f9186fd390df7c34fa20e1e9
Evan Sosenko <razorx@evansosenko.com>
2015-12-21 11:38:09 -0800

    Solve day 19 part 1

--- src/adventofcode/day_19.clj
+++ src/adventofcode/day_19.clj
@@ -1,6 +1,41 @@
 (ns adventofcode.day-19)
 
+(defn parse-replacements
+  "Converts a replacement string, e.g., H => HO, to a vector."
+  [string]
+  (apply vector (rest (re-matches #"(\w+) => (\w+)" string))))
+
+(defn replace-nth
+  "Replace the nth match (zero-indexed) in string."
+  [string match replacement n]
+  (let [split-str (clojure.string/split string (re-pattern match))
+        split-seq (split-at (inc n) split-str)
+        joined-seq (map (partial clojure.string/join match) split-seq)
+        joined-str (clojure.string/join replacement joined-seq)
+        num-matches (dec (count split-str))]
+    (if (zero? num-matches)
+      string
+      (if (and (= match (str (last string)))
+               (not= n num-matches))
+        (str joined-str match)
+        joined-str))))
+
+(defn distinct-replacements
+  "Returns a set of the possible strings formed
+  by making a single replacement."
+  [string match replacement]
+  (set (map (partial replace-nth string match replacement)
+            ((comp range count (partial re-seq (re-pattern match)))
+             string))))
+
 (defn solve
   "Given the input for the day, returns the solution."
   [input]
-  [])
+  (let [input (clojure.string/split-lines input)
+        replacements (map parse-replacements (drop-last 2 input))
+        molecule (last input)]
+    [(count
+      (reduce
+       #(clojure.set/union
+         %1 (apply (partial distinct-replacements molecule) %2))
+       #{} replacements))]))
--- test/adventofcode/day_19_test.clj
+++ test/adventofcode/day_19_test.clj
@@ -1,3 +1,42 @@
 (ns adventofcode.day-19-test
   (:require [clojure.test :refer :all]
             [adventofcode.day-19 :refer :all]))
+
+(deftest parse-replacements-test
+  (is (= ["H" "HO"]   (parse-replacements "H => HO")))
+  (is (= ["Al" "ThF"] (parse-replacements "Al => ThF"))))
+
+(deftest replace-nth-test
+  (is (= "HOH"  (replace-nth "HOH" "S" "H" 0)))
+  (is (= "HOH"  (replace-nth "HOH" "H" "H" 0)))
+  (is (= "HOH"  (replace-nth "HOH" "O" "O" 0)))
+  (is (= "HOH"  (replace-nth "HOH" "H" "H" 1)))
+  (is (= "HOOH" (replace-nth "HOH" "H" "HO" 0)))
+  (is (= "HOHO" (replace-nth "HOH" "H" "HO" 1)))
+  (is (= "OHOH" (replace-nth "HOH" "H" "OH" 0)))
+  (is (= "HOOH" (replace-nth "HOH" "H" "OH" 1)))
+  (is (= "HHHH" (replace-nth "HOH" "O" "HH" 0)))
+  (is (= "HHHHOHO" (replace-nth "HOHOHO" "O" "HH" 0)))
+  (is (= "HOHHHHO" (replace-nth "HOHOHO" "O" "HH" 1)))
+  (is (= "HOOHTOHTHHOH" (replace-nth "HOHTOHTHHOH" "H" "HO" 0)))
+  (is (= "HOHTOHTHOHOH" (replace-nth "HOHTOHTHHOH" "H" "HO" 3)))
+  (is (= "HOHTOHTHHOHO" (replace-nth "HOHTOHTHHOH" "H" "HO" 5))))
+
+(deftest distinct-replacements-test
+  (is (= #{"HOOH" "HOHO"}
+         (distinct-replacements "HOH" "H" "HO")))
+  (is (= #{"OHOH" "HOOH"}
+         (distinct-replacements "HOH" "H" "OH")))
+  (is (= #{"HHHH"}
+         (distinct-replacements "HOH" "O" "HH")))
+  (is (= #{"HOOHOHO" "HOHOOHO" "HOHOHOO"}
+         (distinct-replacements "HOHOHO" "H" "HO")))
+  (is (= #{"OHOHOHO" "HOOHOHO" "HOHOOHO"}
+         (distinct-replacements "HOHOHO" "H" "OH")))
+  (is (= #{"HHHHOHO" "HOHHHHO" "HOHOHHH"}
+         (distinct-replacements "HOHOHO" "O" "HH"))))
+
+(deftest solve-test
+  (testing "part 1"
+    (is (= 4 (first (solve "H => HO\nH => OH\nO => HH\n\nHOH"))))
+    (is (= 7 (first (solve "H => HO\nH => OH\nO => HH\n\nHOHOHO"))))))
