480c52436fff394bd601e4639b2d2b9498b0b6b9
Evan Sosenko <razorx@evansosenko.com>
2015-12-21 22:47:04 -0800

    Solve day 20 part 2

--- test/adventofcode/day_20_test.clj
+++ test/adventofcode/day_20_test.clj
@@ -6,6 +6,11 @@
   (is (= [10 30 40 70 60 120 80 150 130]
          (map house-presents (range 1 10)))))
 
+(deftest house-presents-fifty-test
+  (is (= [11 33 44 77 66 132 88 165 143]
+         (map house-presents-fifty (range 1 10))))
+  (is (= 2376 (house-presents-fifty 100))))
+
 (deftest solve-test
   (testing "part 1"
     (is (= 8 (first (solve "130"))))
