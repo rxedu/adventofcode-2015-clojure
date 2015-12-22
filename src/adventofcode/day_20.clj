2750188a8e98cafebfefc912b6141f4abb5137be
Evan Sosenko <razorx@evansosenko.com>
2015-12-21 22:47:04 -0800

    Solve day 20 part 2

--- src/adventofcode/day_20.clj
+++ src/adventofcode/day_20.clj
@@ -9,12 +9,26 @@
                        (+ ^int %1 ^int %2) %1)]
     (* 10 ^int (reduce presents-fn 0 (range 1 (inc ^int n))))))
 
+(defn house-presents-fifty
+  "Returns the number of presents delivered to the nth house,
+  but elf only delivers to fifty houses."
+  [n]
+  (let [presents-fn #(if (and (zero? ^int (mod n %2))
+                              (>= 50 (/ ^int n %2)))
+                       (+ ^int %1 ^int %2) %1)]
+    (* 11 ^int (reduce presents-fn 0 (range 1 (inc ^int n))))))
+
 (defn solve
   "Given the input for the day, returns the solution."
   [input]
   (let [presents (Integer. (clojure.string/trim-newline input))]
     [(loop [n 1]
        (let [total-house-presents (house-presents n)]
+         (if (< ^int total-house-presents ^int presents)
+           (recur (inc n))
+           n)))
+     (loop [n 1]
+       (let [total-house-presents (house-presents-fifty n)]
          (if (< ^int total-house-presents ^int presents)
            (recur (inc n))
            n)))]))
