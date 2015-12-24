0d038fd789c92c447d9701ec9a10de904c4ff3d4
Evan Sosenko <razorx@evansosenko.com>
2015-12-24 11:16:48 -0800

    Solve day 23 part 1

--- src/adventofcode/day_23.clj
+++ src/adventofcode/day_23.clj
@@ -1,6 +1,62 @@
 (ns adventofcode.day-23)
 
+(def matchers
+  {:hlf #"hlf (\w)"
+   :tpl #"tpl (\w)"
+   :inc #"inc (\w)"
+   :jmp #"jmp ([+-]?\d+)"
+   :jie #"jie (\w), ([+-]\d+)"
+   :jio #"jio (\w), ([+-]\d+)"})
+
+(def parsers
+  (let [convert #(if (re-find #"[+-]?\d+" %) (Integer. %) (keyword %))
+        single-input #(vector (convert %))
+        double-input #(vector (convert %1) (convert %2))]
+    {:hlf single-input
+     :tpl single-input
+     :inc single-input
+     :jmp single-input
+     :jie double-input
+     :jio double-input}))
+
+(def instruction-fns
+  {:hlf #(assoc %1 :idx (inc (:idx %1)) (first %2) (/ ((first %2) %1) 2))
+   :tpl #(assoc %1 :idx (inc (:idx %1)) (first %2) (* 3 ((first %2) %1)))
+   :inc #(assoc %1 :idx (inc (:idx %1)) (first %2) (inc ((first %2) %1)))
+   :jmp #(assoc %1 :idx (+ (first %2) (:idx %1)))
+   :jie #(assoc %1 :idx (if (even? ((first %2) %1))
+                          (+ (second %2) (:idx %1)) (inc (:idx %1))))
+   :jio #(assoc %1 :idx (if (= 1 ((first %2) %1))
+                          (+ (second %2) (:idx %1)) (inc (:idx %1))))})
+
+(defn parse-instruction
+  "Converts an instruction sting to a map."
+  [string]
+  (some (fn [[ins matches]]
+          (and matches (hash-map ins (apply (ins parsers) (rest matches)))))
+        (reduce
+         (fn [new-map [k v]]
+           (assoc new-map k (re-matches v string)))
+         {} matchers)))
+
+(defn run-program
+  "Runs a program of instructions."
+  [instructions]
+  (reduce
+   (fn [{:keys [idx] :as state} ins]
+     (if (not (< -1 idx (count ins)))
+       (reduced state)
+       (let [instruction (first (vec (nth ins idx)))
+             i (first instruction)
+             args (second instruction)]
+         ((i instruction-fns) state args))))
+   {:idx 0 :a 0 :b 0}
+   (repeat instructions)))
+
 (defn solve
   "Given the input for the day, returns the solution."
   [input]
-  [])
+  (let [instructions
+        (map parse-instruction (clojure.string/split-lines input))]
+    (println instructions)
+    [(:b (run-program instructions))]))
--- test/adventofcode/day_23_test.clj
+++ test/adventofcode/day_23_test.clj
@@ -1,3 +1,26 @@
 (ns adventofcode.day-23-test
   (:require [clojure.test :refer :all]
             [adventofcode.day-23 :refer :all]))
+
+(deftest parse-instruction-test
+  (are [ins s] (= ins (parse-instruction s))
+    {:hlf [:a]}    "hlf a"
+    {:tpl [:b]}    "tpl b"
+    {:inc [:a]}    "inc a"
+    {:jmp [4]}     "jmp +4"
+    {:jmp [-10]}   "jmp -10"
+    {:jie [:a  2]} "jie a, +2"
+    {:jio [:b -4]} "jio b, -4"))
+
+(deftest run-program-test
+  (is (= {:idx 2 :a 1 :b 1}
+         (run-program [{:inc [:a]}
+                       {:inc [:b]}])))
+  (is (= {:idx 7 :a 2 :b 3}
+         (run-program [{:inc [:a]}
+                       {:inc [:b]}
+                       {:jie [:b 2]}
+                       {:jmp [-3]}
+                       {:tpl [:b]}
+                       {:hlf [:b]}
+                       {:jio [:a -2]}]))))
