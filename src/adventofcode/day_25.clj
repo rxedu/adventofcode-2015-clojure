d50684c180fc9ea3ae365b27b31a3e2ca68641c3
Evan Sosenko <razorx@evansosenko.com>
2015-12-27 23:25:44 -0800

    Solve day 25 part 1

--- src/adventofcode/day_25.clj
+++ src/adventofcode/day_25.clj
@@ -1,6 +1,50 @@
 (ns adventofcode.day-25)
 
+(defn parse-message
+  "Converts the code request message to a grid point."
+  [string]
+  (reverse (map #(Integer. %)
+                (rest (re-find #"(\d+),\D+(\d+)" string)))))
+
+(defn triangular
+  "Generates lazy seq of triangular numbers."
+  ([] (triangular 0 1))
+  ([sum n]
+   (let [sum (+ sum n)]
+     (cons sum (lazy-seq (triangular sum (inc n)))))))
+
+(def triangular-memo (memoize triangular))
+
+(defn column
+  "Generates a lazy seq of the code indexes for column x."
+  ([x] (column x (nth (triangular-memo) (dec x)) 0))
+  ([x sum n]
+   (let [sum (+ sum n (if (zero? n) 0 (dec x)))]
+     (cons sum (lazy-seq (column x sum (inc n)))))))
+
+(def column-memo (memoize column))
+
+(defn code-index
+  "Given a grid point [column row], returns the code index for that point."
+  [[x y]]
+  (nth (column-memo x) (dec y)))
+
+(defn codes
+  "Returns a lazy seq of the codes."
+  ([] (codes 20151125 0))
+  ([sum n]
+   (let [new-sum (if (zero? n)
+                   sum
+                   (rem (* sum 252533) 33554393))]
+     (cons new-sum (lazy-seq (codes new-sum (inc n)))))))
+
+(defn code-at
+  "Returns the code at the given point."
+  [point]
+  (nth (codes) (dec (code-index point))))
+
 (defn solve
   "Given the input for the day, returns the solution."
   [input]
-  [])
+  (let [point (parse-message (clojure.string/trim-newline input))]
+    [(code-at point)]))
--- test/adventofcode/day_25_test.clj
+++ test/adventofcode/day_25_test.clj
@@ -1,3 +1,44 @@
 (ns adventofcode.day-25-test
   (:require [clojure.test :refer :all]
             [adventofcode.day-25 :refer :all]))
+
+(deftest parse-message-test
+  (is (= [300 42]
+         (parse-message
+          "To continue, please consult the code grid in the manual.
+          Enter the code at row 42, column 300"))))
+
+(deftest triangular-test
+  (is (= [1 3 6 10 15 21] (take 6 (triangular)))))
+
+(deftest column-test
+  (is (= [1 2 4 7 11 16] (take 6 (column 1))))
+  (is (= [3 5 8 12 17]   (take 5 (column 2))))
+  (is (= [6 9 13 18]     (take 4 (column 3))))
+  (is (= [10 14 19]      (take 3 (column 4))))
+  (is (= [15 20]         (take 2 (column 5))))
+  (is (= [21]            (take 1 (column 6)))))
+
+(deftest code-index-test
+  (are [n p] (= n (code-index p))
+    1  [1 1]
+    2  [1 2]
+    3  [2 1]
+    4  [1 3]
+    16 [1 6]
+    19 [4 3]
+    20 [5 2]
+    21 [6 1]))
+
+(deftest codes-test
+  (is (= [20151125 31916031 18749137 16080970 21629792]
+         (take 5 (codes)))))
+
+(deftest code-at-test
+  (are [c x y] (= c (code-at [x y]))
+    20151125 1 1
+    18749137 2 1
+    17289845 3 1
+    31916031 1 2
+    21629792 2 2
+    8057251  2 3))
