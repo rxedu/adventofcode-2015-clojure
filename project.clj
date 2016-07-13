d381569eda4c5aeb161d42d170a432dc0ac2d5ff
Evan Sosenko <razorx@evansosenko.com>
2016-07-12 19:36:58 -0700

    Update combinatorics to 0.1.3

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]
-                 [org.clojure/math.combinatorics "0.1.1"]
+                 [org.clojure/math.combinatorics "0.1.3"]
                  [digest "1.4.4"]]
   :main ^:skip-aot adventofcode.core
   :target-path "target/%s"
