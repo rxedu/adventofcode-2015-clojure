41f08e17183291b7925b030f92c269af38cf81c6
Evan Sosenko <razorx@evansosenko.com>
2016-07-12 19:36:48 -0700

    Update Clojure to 1.8.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/rxedu/adventofcode"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/math.combinatorics "0.1.1"]
                  [digest "1.4.4"]]
